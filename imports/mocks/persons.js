70f504ce4c572c4ceb78701f2f3750588cef67f3
Marcos Andrei Ivanechtchuk <ivmarcos@gmail.com>
2017-09-19 22:30:26 -0300

    tests with crodity-ui

---
+++ imports/mocks/persons.js
@@ -0,0 +1,48 @@
+
+const amanda = 'static/persons/amanda.jpg';
+const paulo = 'static/persons/paulo.jpg';
+const andre = 'static/persons/andre.jpg';
+const luciana = 'static/persons/luciana.jpg';
+const patricia = 'static/persons/patricia.jpg';
+const guilherme = 'static/persons/guilherme.jpg';
+const karina = 'static/persons/karina.jpg';
+
+const persons = [
+  {
+    name: 'Amanda Guedes',
+    location: 'São Paulo - SP',
+    src: amanda,
+  },
+  {
+    name: 'Paulo Oliveira',
+    location: 'São Paulo - SP',
+    src: paulo,
+  },
+  {
+    name: 'André Matias',
+    location: 'Rio de Janeiro - RJ',
+    src: andre,
+  },
+  {
+    name: 'Luciana Guerra',
+    location: 'Jacareí - SP',
+    src: luciana,
+  },
+  {
+    name: 'Patrícia Santos',
+    location: 'São Paulo - SP',
+    src: patricia,
+  },
+  {
+    name: 'Guilherme Viegas',
+    location: 'Estados Unidos',
+    src: guilherme,
+  },
+  {
+    name: 'Karina Mendes',
+    location: 'São Paulo - SP',
+    src: karina,
+  },
+];
+
+export default persons;
--- imports/ui/scenes/LoginScene.jsx
+++ imports/ui/scenes/LoginScene.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import persons from '/imports/mocks/persons';
+
 
 import {
   LoginSocialButton,
   AddCheckButton,
   CrodityIcon,
   Video,
+  FollowersList,
+  FollowersListItem,
 } from 'crodity-ui';
 
 const LoginScene = () => (
@@ -31,6 +35,31 @@ const LoginScene = () => (
     <AddCheckButton onClick={() => console.log('ok')} />
     <AddCheckButton onClick={() => console.log('ok')} added />
     <Video src="https://www.youtube.com/embed/zWk5a-lDe8c" />
+    <div>
+      <FollowersList>
+        {persons.map((person, i) => (
+          <FollowersListItem
+            key={i}
+            added={i % 2 === 0}
+            userName={person.name}
+            locationName={person.location}
+            avatarSrc={person.src}
+            onRequestCheck={e => console.log('check something')}
+            onRequestComment={e => console.log('comment something')}
+            menuActions={[
+              {
+                primaryText: 'action1',
+                onClick: e => console.log('do something'),
+              },
+              {
+                primaryText: 'action2',
+                onClick: e => console.log('do something again'),
+              },
+            ]}
+          />
+        ))}
+      </FollowersList>
+    </div>
   </div>
 
 );
